0666c041fcbff3317ae2013dcffb53eb601602f9
PrestonGraber <prestongraber123@gmail.com>
2021-02-25 10:11:56 -0500

    commit hw4

---
+++ testing/Test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+main(){
+  int x  = 93;
+  int z;
+  z = x / 2;
+ // char result = (char) z;
+
+  printf("the integer is: %d\n",z);
+  return 0;
+}
